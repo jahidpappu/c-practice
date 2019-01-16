51d044c25e93c338de307218f692736eec54aa63
jahid <jahidpappublog@gmail.com>
2019-01-16 19:29:57 +0600

    new

--- c_pratices.c
+++ c_pratices.c
@@ -2,8 +2,7 @@
 //c 
 
 #include<stdio.h>
-int main()
-{
+int main(){
   int x = 10;
   int result = +x; //y =10;
 
@@ -44,5 +43,55 @@ int main()
   printf("x = %d\n", --x); // x = 10
 
   return 0;
+
+}
+
+
+//c 
+
+#include<stdio.h>
+int main()
+{
+  int x = 10;
+  int result = +x; //y =10;
+
+  printf("%d\n", result);
+
+  return 0;
+
+}
+
+
+
+#include<stdio.h>
+int main()
+{
+  int x = 10;
+  int y = x++; //y =10;
+
+  printf("x = %d\n", x);
+  printf("y = %d\n", y);
+
+  return 0;
+
+}
+
+
+
+
+#include<stdio.h>
+int main()
+{
+ int num;
+ printf("Enter an interger : ");
+ scanf("%d", &num);
+if( num % 2 == 0){
+  prinft("even");
+}
+ else(num % 2 != 0){
+    prinft("odd"); 
+ } 
+
+ return 0;
 
 }
